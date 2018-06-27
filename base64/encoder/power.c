99fdafb1584b6db754c84d714fd73526cf76fbc5
l <7uC@protonmail.com>
2018-06-27 14:52:39 -0400

    organized and cleaned

--- base64/encoder/power.c
+++
@@ -1,7 +0,0 @@
-int	power(int base, int exp)
-{
-	if (exp == 0)
-		return (1);
-	else
-		return (base * power(base, exp - 1));
-}
--- base64/encoder/str_len.c
+++
@@ -1,9 +0,0 @@
-int	str_len(char *str)
-{
-	int count;
-
-	count = 0;
-	while (*str++)		// checks while condition then increments
-		count++;
-	return (count);	// no null terminal
-}
